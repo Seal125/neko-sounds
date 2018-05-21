8afe76e8ab4cf384d4c0d7b72f8fab5d1edbec0f
Stephanie <seal12514@gmail.com>
2018-05-21 20:25:58 +0000

    Finished MVP!

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
 $( document ).ready(function(){
-    $("(category box here)").click(
-        $("#122").html(<iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/178581786&color=%234e4d4d&auto_play=true&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"></iframe>));
+    $("#category1").click(function(){
+    });
 });
